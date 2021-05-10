ae4f9824d1a66fac961b6c675047928a525ac4a8
Lacey <lacey.griffith@g.austincc.edu>
2021-05-10 08:19:25 -0500

    add comment functionality, display comments in users/id, posts, posts/id, /comments

--- models/index.js
+++ models/index.js
@@ -1,9 +1,19 @@
+//
 const User = require('./User')
 const Post = require('./Post')
 const Vote = require('./Vote')
+const Comment = require('./Comment')
 
 //create associations
-User.belongsToMany(Post, {
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  User.belongsToMany(Post, {
     through: Vote,
     as: 'voted_posts',
     foreignKey: 'user_id'
@@ -31,4 +41,20 @@ Post.hasMany(Vote, {
     foreignKey: 'post_id'
 });
 
-module.exports = {User, Post, Vote};
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {User, Post, Vote, Comment};
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 
 //GET all posts
@@ -12,6 +12,13 @@ router.get('/', (req, res) => {
             ['created_at', 'DESC']
         ],
         include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id','user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },{
             model: User,
             attributes: ['username']
         }]
@@ -32,6 +39,14 @@ router.get('/:id', (req, res) => {
         [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
         ],
         include: [{
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id','user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
             model: User,
             attributes: ['username']
         }]
