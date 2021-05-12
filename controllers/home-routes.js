2e9dfca74b63e4a978f5ef09c2a629eec38f598c
Lacey <lacey.griffith@g.austincc.edu>
2021-05-12 13:45:00 -0500

    display forms, login/logout based on session

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -25,11 +25,12 @@ Post.findAll({
         attributes: ['username']
     }
     ]
-})
-    .then(dbPostData => {
+}).then(dbPostData => {
     // pass a single post object into the homepage template
     const posts = dbPostData.map(post => post.get({ plain: true }));
-    res.render('homepage', { posts });
+    res.render('homepage', { 
+      posts,
+      loggedIn: req.session.loggedIn });
     })
     .catch(err => {
     console.log(err);
@@ -83,7 +84,9 @@ router.get('/login', (req, res) => {
         const post = dbPostData.get({ plain: true });
   
         // pass data to template
-        res.render('single-post', { post });
+        res.render('single-post', { 
+          post,
+        loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
