a1b75cb756ed6c7aca68d71d8858a3ac5e18d294
Lacey <lacey.griffith@g.austincc.edu>
2021-05-18 14:42:56 -0500

    edit post, delete post has an error

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,64 +1,25 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { Post, User, Comment, Vote } = require('../models');
 const withAuth = require('../utils/auth');
-const {
-  Post,
-  User,
-  Comment
-} = require('../models');
 
+// get all posts for dashboard
 router.get('/', withAuth, (req, res) => {
+  console.log(req.session);
+  console.log('======================');
   Post.findAll({
-      where: {
-        // use the ID from the session
-        user_id: req.session.user_id
-      },
-      attributes: [
-        'id',
-        'post_url',
-        'title',
-        'created_at',
-        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-      ],
-      include: [{
-          model: Comment,
-          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
-          model: User,
-          attributes: ['username']
-        }
-      ]
-    })
-    .then(postData => {
-      // serialize data before passing to template
-      const posts = postData.map(post => post.get({
-        plain: true
-      }));
-      res.render('dashboard', {
-        posts,
-        loggedIn: true
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
-
-router.get('/edit/:id', withAuth, (req, res) => {
-  Post.findOne({
     where: {
-      id: req.params.id
+      user_id: req.session.user_id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at',
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
     ],
-    include: [{
+    include: [
+      {
         model: Comment,
         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
         include: {
@@ -71,16 +32,56 @@ router.get('/edit/:id', withAuth, (req, res) => {
         attributes: ['username']
       }
     ]
-  }).then(postData => {
-    const post = postData.get({
-      plain: true
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
     })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
-    res.render('edit-post', {
-      post,
-      loggedIn: true
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findByPk(req.params.id, {
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (dbPostData) {
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('edit-post', {
+          post,
+          loggedIn: true
+        });
+      } else {
+        res.status(404).end();
+      }
     })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
-  }).catch(err => res.status(500).json(err))
-})
 module.exports = router;
